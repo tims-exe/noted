2502b72b6d13184778827043bc674f4357da65f0
tims-exe <timonkjohn@gmail.com>
2025-07-28 20:48:02 +0530

    auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { NextAuthOptions } from "next-auth"
+import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from "./prisma"
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ],
+    callbacks: {
+        session: async ({ session, token }) => {
+            if (session?.user) {
+                session.user.id = token.sub!
+            }
+            return session 
+        }, 
+        jwt: async ({ user, token }) => {
+            if (user) {
+                token.uid = user.id
+            }
+            return token
+        },
+    },
+    session: {
+        strategy: 'jwt',
+    }
+}
